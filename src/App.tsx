55ac25330bfda43c3c13f7fdf8f696c1495687c5
tuprikov <tuprikov@gmail.com>
2026-04-06 11:19:27 +0200

    App scaffolding.

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div className="mx-auto flex w-[640px] flex-col py-4">
+            <div className="flex flex-col gap-2 rounded-xl bg-white p-4">
+                {/* Header */}
+                <header>
+                    <h1 className="mb-4 border-b border-gray-300 text-2xl font-bold">
+                        Journal App
+                    </h1>
+                    <p>
+                        Write down your thoughts, celebrate small wins, and grow
+                        through daily reflection in a safe, uplifting space.
+                    </p>
+                </header>
+                {/* Tabs */}
+                {/* Form */}
+            </div>
+        </div>
+    )
+}
+
+export default App
